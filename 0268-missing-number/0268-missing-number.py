77067f0ecc00a678f22ab6dba84d518e0e75e8d6
SWARNENDU MAJUMDER <159484122+swarnendu-labs@users.noreply.github.com>
2026-07-17 21:03:39 +0530

    Time: 1 ms (64.97%), Space: 20.3 MB (74.83%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        ans = len(nums)
+        for i, num in enumerate(nums):
+            ans ^= i ^ num
+        return ans
