43b9c50be4505a19a7f0b029f1875d60cb86a404
SWARNENDU MAJUMDER <159484122+swarnendu-labs@users.noreply.github.com>
2026-07-20 13:55:34 +0530

    Time: 7 ms (58.44%), Space: 19.7 MB (48.15%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        m, n = len(grid), len(grid[0])
+        total = m * n
+        k %= total
+
+        res = [[0] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                idx = (i * n + j + k) % total
+                ni, nj = divmod(idx, n)
+                res[ni][nj] = grid[i][j]
+
+        return res
