98eaf1569584b1112990e11c14345dfe45e48ad0
SWARNENDU MAJUMDER <159484122+swarnendu-labs@users.noreply.github.com>
2026-08-04 19:18:24 +0530

    Time: 0 ms (100%), Space: 19.2 MB (90.85%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        rows = [
+            set("qwertyuiop"),
+            set("asdfghjkl"),
+            set("zxcvbnm")
+        ]
+
+        ans = []
+
+        for word in words:
+            w = set(word.lower())
+            for row in rows:
+                if w <= row:
+                    ans.append(word)
+                    break
+
+        return ans
